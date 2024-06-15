fafd31e07b27349d1d95745cdedca70caa94039e
mayurb6706 <mayurbhosale6706@gmail.com>
2024-06-15 19:40:56 +0530

    Euraka Service Registery

---
+++ Euraka-Service/src/test/java/com/cwm/eureka/EurakaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwm.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurakaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
