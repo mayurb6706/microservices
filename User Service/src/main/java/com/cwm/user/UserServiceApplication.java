02737e77d2462bec73ab979b09a2632f466e4457
mayurb6706 <mayurbhosale6706@gmail.com>
2024-06-15 19:48:35 +0530

    Added Eureka Discovery Client

--- User Service/src/main/java/com/cwm/user/UserServiceApplication.java
+++ User Service/src/main/java/com/cwm/user/UserServiceApplication.java
@@ -3,11 +3,13 @@ package com.cwm.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan("com.cwm.user.entity")
 @EnableJpaRepositories("com.cwm.user.repository")
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
