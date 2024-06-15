fd233d259ef944a1e0de9d1d2f21edc75b785b5b
mayurb6706 <mayurbhosale6706@gmail.com>
2024-06-15 19:53:41 +0530

    Added Euraka Discovery Client

--- Hotel-Service/src/main/java/com/cwm/hotel/HotelServiceApplication.java
+++ Hotel-Service/src/main/java/com/cwm/hotel/HotelServiceApplication.java
@@ -3,11 +3,13 @@ package com.cwm.hotel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.cwm.hotel.repository")
 @EntityScan("com.cwm.hotel.entity")
+@EnableDiscoveryClient
 public class HotelServiceApplication {
 
 	public static void main(String[] args) {
