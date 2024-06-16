c693877bf2b00eccbd9466d1e1e89c35061bd95a
mayurb6706 <mayurbhosale6706@gmail.com>
2024-06-16 16:10:31 +0530

    API Gatway port Check

--- Rating-Service/src/main/java/com/cwm/rating/RatingServiceApplication.java
+++ Rating-Service/src/main/java/com/cwm/rating/RatingServiceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EntityScan("com.cwm.rating.entity")
 @EnableJpaRepositories("com.cwm.rating.repository")
-@EnableDiscoveryClient
 public class RatingServiceApplication {
 
 	public static void main(String[] args) {
--- User Service/src/main/java/com/cwm/user/service/impl/UserServiceImpl.java
+++ User Service/src/main/java/com/cwm/user/service/impl/UserServiceImpl.java
@@ -1,16 +1,13 @@
 package com.cwm.user.service.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.cwm.user.entity.User;
 import com.cwm.user.exception.UserDetailsNotFoundException;
-import com.cwm.user.model.RatingResponse;
 import com.cwm.user.repository.UserRepository;
 import com.cwm.user.service.UserService;
-import com.cwm.user.service.external.RatingService;
 
 import lombok.AllArgsConstructor;
 
@@ -20,7 +17,6 @@ public class UserServiceImpl implements UserService {
 
 	private UserRepository userRepo;
 	
-	private RatingService ratingService;
 	
 	//Add new User
 	@Override
@@ -33,9 +29,6 @@ public class UserServiceImpl implements UserService {
 	public User getUserById(Long id) {
 		User user=this.userRepo.findById(id).orElseThrow(()->
 		new UserDetailsNotFoundException("User is not exist with given parameter!"));
-		
-		//Get the rating of user 
-		
 		return user;
 	}
 
